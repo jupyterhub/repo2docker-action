b270360220059063a184936ae51541dac0e62edd
rimas silkaitis <neovintage@github.com>
2019-10-21 22:17:16 -0700

    another test

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -5,8 +5,6 @@
 : ${REGISTRY_PASSWORD:=$GITHUB_TOKEN}
 
 echo "Building binder"
+ls -al
 jupyter-repo2docker --no-run --ref $GITHUB_SHA .
 
-#docker login -u="$REGISTRY_USERNAME" -p="$REGISTRY_PASSWORD"
-#docker tag IMAGE_ID docker.pkg.github.com/neovintage/notebooks/IMAGE_NAME:VERSION
-#docker push docker.pkg.github.com/neovintage/notebooks/IMAGE_NAME:VERSION
