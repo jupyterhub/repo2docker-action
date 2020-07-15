8104c903a95b60e7011dc225f8d8ac2f9b095751
Hamel Husain <hamel.husain@gmail.com>
2020-07-15 16:14:49 -0700

    update docs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,12 @@
 
 # THIS FILE IS FOR LOCAL TESTING
 
-docker build -t hamelsmu/repo2docker .
+docker build -t github/repo2docker-test .
 
-INPUT_DOCKER_REGISTRY_USERNAME="hamelsmu"
+INPUT_DOCKER_REGISTRY_USERNAME="${DOCKER_USERNAME}"
 INPUT_DOCKER_REGISTRY_PASSWORD="${DOCKER_PASSWORD}"
-INPUT_IMAGE_NAME="hamelsmu/repo2docker-test"
+INPUT_IMAGE_NAME="github/repo2docker-test"
 GITHUB_SHA="c0d285f7eef547b16b28fece79a866894db08c04"
-
 
 # This container builds the container with repo2docker
 docker run \
@@ -17,6 +16,5 @@ docker run \
 -e INPUT_DOCKER_PASSWORD=$INPUT_DOCKER_REGISTRY_PASSWORD \
 -e INPUT_IMAGE_NAME=$INPUT_IMAGE_NAME \
 -e GITHUB_SHA=${GITHUB_SHA} \
--e GITHUB_ACTOR="hamelsmu" \
 -v ${PWD}:/github/workspace \
-hamelsmu/repo2docker
+github/repo2docker-test
