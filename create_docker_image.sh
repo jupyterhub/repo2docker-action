b88ca663a82aed19f26995baeffa9a552f9ecf9d
YuviPanda <yuvipanda@gmail.com>
2023-05-08 17:44:10 +0200

    Volume mount image-tests

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -133,15 +133,15 @@ echo "::endgroup::"
 if [ -d "${PWD}/image-tests" ]; then
     echo "::group::Run tests found in image-tests/"
     # We pass in bash that is run inside the built container, so watch out for quoting.
-    docker run -u 1000 -w ${REPO_DIR} \
+    docker run -u 1000 -v "${PWD}/image-tests:/tmp/image-tests" -w ${REPO_DIR} \
         ${SHA_NAME} /bin/bash -c '
         export PYTEST_FLAGS="";
 
         # If there is a requirements.txt file inside image-tests, install it.
         # Useful if you want to install a bunch of pytest packages.
-        [ -f image-tests/requirements.txt ] && \
+        [ -f /tmp/image-tests/requirements.txt ] && \
             echo "Installing from image-tests/requirements.txt..." && \
-            python3 -m pip install --no-cache -r image-tests/requirements.txt;
+            python3 -m pip install --no-cache -r /tmp/image-tests/requirements.txt;
 
         # If pytest is not already installed in the image, install it.
         which py.test > /dev/null || \
@@ -150,13 +150,13 @@ if [ -d "${PWD}/image-tests" ]; then
 
         # If there are any .ipynb files in image-tests, install pytest-notebook
         # if necessary, and set PYTEST_FLAGS so notebook tests are run.
-        ls image-tests/*.ipynb > /dev/null && \
+        ls /tmp/image-tests/*.ipynb > /dev/null && \
             echo "Found notebooks, using pytest-notebook to run them..." && \
             export PYTEST_FLAGS="--nb-test-files ${PYTEST_FLAGS}" && \
             python3 -c "import pytest_notebook" 2> /dev/null || \
                 python3 -m pip install --no-cache pytest-notebook > /dev/null;
 
-        py.test ${PYTEST_FLAGS} image-tests/
+        py.test ${PYTEST_FLAGS} /tmp/image-tests/
     '
     echo "::endgroup::"
 fi
